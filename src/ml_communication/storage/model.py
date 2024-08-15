0eb71960190ac93ac1cfc72fcf09904d304c1772
Rodrigo Castaldoni <castaldoniro@gmail.com>
2024-08-15 11:26:28 -0300

    feat: improve storage interface
    Signed-off-by: Rodrigo Castaldoni <castaldoniro@gmail.com>

--- src/ml_communication/storage/model.py
+++ src/ml_communication/storage/model.py
@@ -5,42 +5,44 @@ from abc import ABC, abstractmethod
 
 class ModelStorage(ABC):
     @abstractmethod
-    def add(self, path: Path, model: Any) -> bool:
+    def add(self, filename: str, model: Any) -> str | None:
         pass 
 
     @abstractmethod
-    def delete(self, path: Path) -> bool:
+    def delete(self, key: str) -> bool:
         pass
     
     @abstractmethod
-    def get(self, path: Path) -> Any | None:
+    def get(self, key: str) -> Any | None:
         pass
 
 
 class FileSystemModelStorage(ModelStorage):
 
-    def add(self, path: Path, model: Any) -> bool:
+    def add(self, filename: Path, model: Any) -> str | None:
         try:
+            path = (Path("tmp") / filename)
+            path.parent.mkdir(parents=True, exist_ok=True)
             with path.open(mode="wb") as f:
                 pickle.dump(model, f)
 
         except Exception as e:
             print(e)
-            return False
+            return None
         else:
-            return True
+            return str(path)
 
-    def delete(self, path: Path) -> bool:
+    def delete(self, key: str) -> bool:
         try:
-            path.unlink(missing_ok=True)
-
+            Path(key).unlink(missing_ok=True)
         except Exception as e:
             print(e)
             return False
         else:
             return True
 
-    def get(self, path: Path) -> Any | None:
+    def get(self, key: str) -> Any | None:
+        path = Path(key)
         if path.exists():
             with path.open(mode="rb") as f:
                 return pickle.load(f)
@@ -50,23 +52,23 @@ class InMemoryModelStorage(ModelStorage):
     def __init__(self):
         self.data = {}
 
-    def add(self, path: Path, model: Any) -> bool:
+    def add(self, filename: str, model: Any) -> str | None:
         try:
-            self.data[path] = model
+            self.data[filename] = model
+        except Exception as e:
+            print(e)
+            return None
+        else:
+            return filename
+
+    def delete(self, key: str) -> bool:
+        try:
+            del self.data[key]
         except Exception as e:
             print(e)
             return False
         else:
             return True
 
-    def delete(self, path: Path) -> bool:
-        try:
-            del self.data[path]
-        except Exception as e:
-            print(e)
-            return False
-        else:
-            return True
-
-    def get(self, path: Path) -> Any | None:
-        return self.data.get(path, None) 
+    def get(self, key: str) -> Any | None:
+        return self.data.get(key, None) 
